4f7ab0a14fed8211006ecca64fd90b7f3c009e9d
Rudi Bruchez <rudi@babaluga.com>
2018-05-27 12:19:24 +0200

    First commit

---
+++ diagnostics/waiting_tasks.sql
@@ -0,0 +1,23 @@
+SELECT
+	s.session_id
+   ,CAST(s.login_time AS DATETIME2(0)) AS login_time
+   ,s.host_name
+   ,s.program_name
+   ,s.login_name
+   ,s.status
+   ,wt.wait_type
+   ,wt.wait_duration_ms
+   ,wt.blocking_session_id
+FROM sys.dm_os_waiting_tasks wt
+JOIN sys.dm_exec_sessions s
+	ON wt.session_id = s.session_id
+WHERE s.session_id > 50
+AND wt.wait_type NOT IN (
+	'XE_LIVE_TARGET_TVF',
+	'BROKER_TASK_STOP',
+	'XE_DISPATCHER_WAIT',
+	'HADR_WORK_QUEUE',
+	'SP_SERVER_DIAGNOSTICS_SLEEP',
+	'PREEMPTIVE_XE_DISPATCHER'
+)
+ORDER BY s.session_id;
