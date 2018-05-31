77fcfe9d6bd8a5d81e08a338f83f3bc987417d30
Rudi Bruchez <rudi@babaluga.com>
2018-05-31 12:16:38 +0200

    added script

---
+++ diagnostics/database-health/forwarded-records.sql
@@ -0,0 +1,45 @@
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+GO
+
+IF OBJECT_ID('tempdb..#HeapList') IS NOT NULL
+    DROP TABLE #HeapList
+
+CREATE TABLE #HeapList
+    (
+    object_name sysname
+    ,page_count int
+    ,avg_page_space_used_in_percent float
+    ,record_count int
+    ,forwarded_record_count int
+    )
+
+DECLARE HEAP_CURS CURSOR FOR
+    SELECT object_id
+    FROM sys.indexes i
+    WHERE index_id = 0
+
+DECLARE @IndexID int
+
+OPEN HEAP_CURS
+FETCH NEXT FROM HEAP_CURS INTO @IndexID
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    INSERT INTO #HeapList
+    SELECT object_name(object_id) as ObjectName
+        ,page_count
+        ,avg_page_space_used_in_percent
+        ,record_count
+        ,forwarded_record_count
+    FROM sys.dm_db_index_physical_stats (db_id(), @IndexID,  0, null,  'DETAILED'); 
+
+    FETCH NEXT FROM HEAP_CURS INTO @IndexID
+END
+
+CLOSE HEAP_CURS
+DEALLOCATE HEAP_CURS
+
+SELECT *
+FROM #HeapList
+WHERE forwarded_record_count > 0
+ORDER BY 1
