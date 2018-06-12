95cede3a779218911c8422521295a0ba9f4c8be7
Rudi Bruchez <rudi@babaluga.com>
2018-06-12 11:31:27 +0200

    Update performance-counters.sql

--- diagnostics/performance-counters.sql
+++ diagnostics/performance-counters.sql
@@ -11,8 +11,8 @@ FROM sys.dm_os_performance_counters WITH (NOLOCK)
 WHERE [object_name] LIKE N'%Buffer%' -- Handles named instances
 AND counter_name IN (
 	N'Page life expectancy', 
-	N'Buffer Cache Hit Ratio',
-	N'Buffer Cache Hit Ratio base'
+	N'Buffer cache hit ratio',
+	N'Buffer cache hit ratio base'
 )	
 OPTION (RECOMPILE);
 -- todo : 100 * 'Buffer Cache Hit Ratio' / 'Buffer Cache Hit Ratio base'
