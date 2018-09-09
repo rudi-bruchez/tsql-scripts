b22458b1586ef43c6cac7eb3c219fd73cf22360e
Rudi Bruchez <rudi@babaluga.com>
2018-09-09 12:41:38 +0200

    improvements

--- database-metadata/compressed-objects.sql
+++ database-metadata/compressed-objects.sql
@@ -1,9 +1,9 @@
-SELECT OBJECT_NAME(i.[object_id]) AS [ObjectName]
+SELECT OBJECT_NAME(i.[object_id]) AS [Table]
     ,i.[index_id] AS [IndexID]
     ,i.[name] AS [IndexName]
     ,i.[type_desc] AS [IndexType]
-    ,p.partition_number
-	,p.data_compression_desc
+    ,p.partition_number AS [partition]
+	,p.data_compression_desc AS [compression]
 FROM [sys].[indexes] i
 JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
 WHERE p.data_compression_desc <> 'NONE'
--- database-metadata/index-usage.sql
+++ database-metadata/index-usage.sql
@@ -13,14 +13,15 @@ SELECT
 	CAST(last_user_seek AS DATETIME2(0)) AS last_seek, 
 	CAST(last_user_scan AS DATETIME2(0)) AS last_scan, 
 	CAST(last_user_update AS DATETIME2(0)) AS last_upd,
-	ps.page_count
+	FORMAT(ps.page_count * 8.192, 'N', 'fr-fr') as size_kb,
+	i.index_id
 FROM sys.dm_db_index_usage_stats ius
 JOIN sys.indexes i ON ius.object_id = i.object_id AND ius.index_id = i.index_id
 JOIN sys.tables t ON i.object_id = t.object_id
-CROSS APPLY sys.dm_db_index_physical_stats(DB_ID(), i.object_id, i.index_id, NULL , N'LIMITED') AS ps
+CROSS APPLY (SELECT SUM(page_count) as page_count FROM sys.dm_db_index_physical_stats(DB_ID(), i.object_id, i.index_id, NULL , N'LIMITED')) AS ps
 WHERE ius.database_id = DB_ID()
 AND ius.object_id = OBJECT_ID('<TABLE NAME>')
 -- AND user_seeks = 0
 AND i.type_desc = N'NONCLUSTERED'
 AND i.is_primary_key = 0
-ORDER BY tbl;
+ORDER BY tbl, ps.page_count DESC;
