340119191b637d0658237d5f1e80b19361489efc
Rudi Bruchez <rudi@babaluga.com>
2018-09-17 23:05:26 +0200

    adding new scripts

---
+++ database-administration/ddl-generation/revoke-proc-privileges.sql
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------
+-- Code to generate REVOKE commands on EXEC privileges for
+-- every procedure and for every database users and roles
+--
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+SELECT 'REVOKE EXEC ON [' + SCHEMA_NAME(p.schema_id) + '].[' + P.name + '] TO [' + dp.name + ']'
+FROM sys.procedures p
+CROSS JOIN sys.database_principals dp
+WHERE p.type IN ('P', 'PC')
