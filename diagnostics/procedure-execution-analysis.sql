e7bf020dbc418468fb68bf659af96e525a196cd1
Rudi Bruchez <rudi@babaluga.com>
2018-10-11 14:58:19 +0200

    improvements

--- diagnostics/procedure-execution-analysis.sql
+++ diagnostics/procedure-execution-analysis.sql
@@ -5,6 +5,29 @@
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
 
+-- global
+SELECT 
+	OBJECT_NAME(ps.object_id) as procedure_name,
+	ps.cached_time, 
+	ps.last_execution_time,
+	DATEDIFF(day, ps.cached_time, ps.last_execution_time) as days_in_cache,
+	ps.execution_count,
+	ps.execution_count / COALESCE(NULLIF(DATEDIFF(hour, ps.cached_time, ps.last_execution_time), 0), 1) AS execution_per_hour,
+	ps.total_worker_time / 1000 / 1000 AS total_worker_time_sec,
+	REPLACE(REPLACE((CONVERT(varchar, CONVERT(money, ps.last_logical_reads), 1)), ',', ' '), '.00', '') AS last_logical_reads,
+	REPLACE(REPLACE((CONVERT(varchar, CONVERT(money, ps.max_logical_reads), 1)), ',', ' '), '.00', '') AS max_logical_reads,
+	(CAST(ps.max_worker_time as bigint) / 1000) / 1000.0 / 60 AS max_worker_time_minutes,
+	(CAST(ps.max_elapsed_time as bigint) / 1000) / 1000.0 / 60 AS max_elapsed_time_minutes,
+	qp.query_plan,
+	CAST(qp.query_plan as XML) as text_query_plan
+FROM sys.dm_exec_procedure_stats ps
+CROSS APPLY sys.dm_exec_query_plan(ps.plan_handle) qp
+WHERE ps.database_id = DB_ID()
+-- AND ps.object_id = OBJECT_ID('<PROCEDURE NAME>')
+ORDER BY execution_per_hour DESC
+OPTION (RECOMPILE);
+
+-- detailed
 SELECT 
 	OBJECT_NAME(ps.object_id) as procedure_name,
 	qs.plan_generation_num,
