e9ce4622855a3f1cfcad87c002a2cf594dc778b2
Rudi Bruchez <rudi@babaluga.com>
2018-10-11 16:05:10 +0200

    new queries

---
+++ security/sysadmin-logins.sql
@@ -0,0 +1,14 @@
+SELECT  p.name AS LoginName ,
+        --p.type ,
+        p.type_desc ,
+        p.is_disabled,
+        CONVERT(VARCHAR(10),p.create_date ,101) AS [created],
+        CONVERT(VARCHAR(10),p.modify_date , 101) AS [update]
+FROM    sys.server_principals p
+JOIN sys.syslogins s ON p.sid = s.sid
+WHERE   p.type_desc IN ('SQL_LOGIN', 'WINDOWS_LOGIN', 'WINDOWS_GROUP')
+        -- Logins that are not process logins
+        AND p.name NOT LIKE '##%'
+        -- Logins that are sysadmins
+        AND s.sysadmin = 1
+ORDER BY LoginName;
