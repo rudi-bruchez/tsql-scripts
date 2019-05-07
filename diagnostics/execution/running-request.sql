fbc84876d773a741783e2ed2249cacc7db120c85
Rudi Bruchez <rudi@babaluga.com>
2019-05-07 10:46:47 +0200

    improvements

--- diagnostics/execution/running-request.sql
+++ diagnostics/execution/running-request.sql
@@ -3,14 +3,16 @@
 SELECT r.session_id, s.login_name, s.host_name, r.start_time, r.status, r.command, t.text,
 	DB_NAME(r.database_id) as db, r.wait_type, r.wait_time, r.last_wait_type,
 	r.open_resultset_count, r.cpu_time, r.total_elapsed_time,
-	r.reads, r.writes, r.logical_reads, r.row_count, r.granted_query_memory
+	r.reads, r.writes, r.logical_reads, r.row_count, r.granted_query_memory, r.blocking_session_id
 FROM sys.dm_exec_requests r
 JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
 OUTER APPLY sys.dm_exec_sql_text(r.sql_handle) t
 WHERE r.session_id > 50
 AND r.command NOT IN ('VDI_CLIENT_WORKER', 'PARALLEL REDO TASK', 'UNKNOWN TOKEN', 'PARALLEL REDO HELP TASK', 
-	'BRKR TASK', 'DB STARTUP', 'TASK MANAGER', 'HADR_AR_MGR_NOTIFICATION_WORKER') -- removing AlwaysOn processes
+	'BRKR TASK', 'DB STARTUP', 'TASK MANAGER', 'HADR_AR_MGR_NOTIFICATION_WORKER') -- removing uninteresting processes
 AND t.text NOT IN ('sp_server_diagnostics')
+AND r.wait_type NOT IN ('BROKER_RECEIVE_WAITFOR')
+AND r.session_id <> @@SPID
 OPTION (RECOMPILE);
 
 -- short version
@@ -68,5 +70,7 @@ WHERE r.session_id > 50
 AND r.command NOT IN ('VDI_CLIENT_WORKER', 'PARALLEL REDO TASK', 'UNKNOWN TOKEN', 'PARALLEL REDO HELP TASK', 
 	'BRKR TASK', 'DB STARTUP', 'TASK MANAGER', 'HADR_AR_MGR_NOTIFICATION_WORKER') -- removing AlwaysOn processes
 AND t.text NOT IN ('sp_server_diagnostics')
+AND r.wait_type NOT IN ('BROKER_RECEIVE_WAITFOR')
+AND r.session_id <> @@SPID
 ORDER BY total_elapsed_time DESC
 OPTION (RECOMPILE);
