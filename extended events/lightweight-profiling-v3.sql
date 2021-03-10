064a4d62b832190dcad87036d7c5c74ef8ebafcb
Rudi Bruchez <rudi@babaluga.com>
2021-03-10 13:20:20 +0100

    xevent for lightweight query profile

---
+++ extended events/lightweight-profiling-v3.sql
@@ -0,0 +1,18 @@
+-- on SQL Server 2019 and 2017 CU 14
+
+SELECT OBJECT_ID('<MY PROCEDURE>');
+GO
+
+CREATE EVENT SESSION [plans_proc] ON SERVER 
+ADD EVENT sqlserver.query_post_execution_plan_profile(
+    ACTION(
+		sqlserver.plan_handle,
+		sqlserver.session_id,
+		sqlserver.sql_text)
+    WHERE (
+		[database_name]=N'<DB NAME>' AND 
+		[object_id]=1234)
+		)
+ADD TARGET package0.ring_buffer(SET max_memory=(25600))
+WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
+GO
