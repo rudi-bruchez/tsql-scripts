02cc81128eb3536dc5019e4216ce583516fd2d33
Rudi Bruchez <rudi@babaluga.com>
2021-05-05 17:24:58 +0200

    sqlos memory draft

---
+++ server-metadata/sqlos_memory.sql
@@ -0,0 +1,5 @@
+SELECT
+	CAST(virtual_address_space_reserved_kb / 1024.0 / 1024 as numeric(10, 2)) as [reserved_gb],
+	CAST(virtual_address_space_committed_kb / 1024.0 / 1024 as numeric(10, 2)) as [committed_gb],
+	*
+FROM sys.dm_os_process_memory
