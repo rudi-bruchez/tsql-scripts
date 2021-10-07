4914ebf097c0f4fdb1d48d78eb30ed36eb13a58a
Rudi Bruchez <rudi@babaluga.com>
2021-10-07 10:41:44 +0200

    separating queries

---
+++ diagnostics/Memory/optimize-for-adhoc-workloads.sql
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------
+-- analysis for optimize for adhoc workloads    --
+
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+SELECT
+	'total' as type,
+	CAST(SUM(CAST(cp.size_in_bytes as bigint)/1024.00) / 1024 as decimal(10, 2)) AS [Plan Size in MB]
+FROM sys.dm_exec_cached_plans AS cp WITH (READUNCOMMITTED)
+WHERe cp.objtype <> 'Proc'
+UNION ALL
+SELECT
+	'usecounts_1' as type,
+	CAST(SUM(CAST(cp.size_in_bytes as bigint)/1024.00) / 1024 as decimal(10, 2)) AS [Plan Size in MB]
+FROM sys.dm_exec_cached_plans AS cp WITH (READUNCOMMITTED)
+WHERE cp.usecounts = 1 AND cp.objtype <> 'Proc'
+ORDER BY type
+OPTION (RECOMPILE, MAXDOP 1);
