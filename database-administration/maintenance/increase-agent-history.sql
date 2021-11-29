7ea457a66ccaf2c9ac785894a707b99cb5d6e5cf
Rudi Bruchez <rudi@babaluga.com>
2021-11-29 13:39:25 +0100

    maintenance folder

---
+++ database-administration/maintenance/increase-agent-history.sql
@@ -0,0 +1,7 @@
+-- increase agent job history to 10,000 rows, 500 max per job
+
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_set_sqlagent_properties @jobhistory_max_rows=10000, 
+		@jobhistory_max_rows_per_job=500
+GO
