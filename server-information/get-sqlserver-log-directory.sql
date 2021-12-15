9dd40c760e910e1e9667b2a9439c5b4227cff4e1
Rudi Bruchez <rudi@babaluga.com>
2021-12-15 10:37:37 +0100

    find log folder

---
+++ server-information/get-sqlserver-log-directory.sql
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------
+-- find the path of the SQL Serverlog directory
+--
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+
+SELECT 
+	LEFT(CAST(SERVERPROPERTY('ErrorLogFileName') as nvarchar(max)), 
+		LEN(CAST(SERVERPROPERTY('ErrorLogFileName') as nvarchar(max))) - LEN('ERRORLOG'));
