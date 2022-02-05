26d2d5649a1054b5f833cd88539d7c352b8736ad
Rudi Bruchez <rudi@babaluga.com>
2022-02-05 11:57:51 +0100

    tempdb

--- database-information/compression/compressed-objects.sql
+++ database-information/compression/compressed-objects.sql
@@ -4,6 +4,9 @@
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
 
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+GO
+
 SELECT 
 	CONCAT(OBJECT_SCHEMA_NAME(i.[object_id]), '.', OBJECT_NAME(i.[object_id])) AS [Table]
     ,i.[index_id] AS [IndexID]
@@ -15,4 +18,5 @@ FROM [sys].[indexes] i
 JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
 WHERE p.data_compression_desc <> 'NONE'
 AND i.object_id NOT IN (SELECT object_id FROM sys.objects WHERE is_ms_shipped = 1)
-ORDER BY [Table];
+ORDER BY [Table]
+OPTION (RECOMPILE, MAXDOP 1);
