4ddd346c2c4749db18498bca7811f181cf592adf
Rudi Bruchez <rudi@babaluga.com>
2022-04-04 13:35:54 +0200

    table information

--- database-information/tables-information/tables-with-LOB.sql
+++ database-information/tables-information/tables-with-LOB.sql
@@ -1,9 +1,21 @@
 -----------------------------------------------------------------
--- table with TEXT or IMAGE types
+-- analyze table allocations where LOB are present
 --
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
 
+;WITH [buffer] AS (
+	SELECT 
+		bd.allocation_unit_id,
+		AVG(bd.free_space_in_bytes) as avg_free_space_in_pages,
+		AVG(bd.row_count) as avg_row_count,
+		COUNT(*) as pages_in_buffer
+	FROM sys.dm_os_buffer_descriptors bd
+	WHERE database_id = DB_ID()
+	AND bd.page_type = 'DATA_PAGE'
+	AND bd.page_level = 0
+	GROUP BY bd.allocation_unit_id
+)
 SELECT DISTINCT
     t.NAME AS TableName,
 	STUFF((
@@ -12,33 +24,32 @@ SELECT DISTINCT
 		WHERE c.object_id = t.object_id 
 		AND c.system_type_id IN (SELECT system_type_id
 			FROM sys.types
-			WHERE name IN (N'text', N'image', N'ntext'))
+			WHERE name IN (N'varchar', N'nvarchar', N'varbinary', N'text', N'ntext', N'image')
+			AND c.max_length = -1
+		)
 		FOR XML PATH('')
 	), 1, 2, '') as [columns],
     i.name as indexName,
     p.[Rows],
 	a.type_desc,
-    --sum(a.total_pages) as TotalPages, 
     a.used_pages,
-    t.text_in_row_limit
-    --sum(a.data_pages) as DataPages,
-    --(sum(a.total_pages) * 8) / 1024 as TotalSpaceMB, 
-    --(sum(a.used_pages) * 8) / 1024 as UsedSpaceMB, 
-    --(sum(a.data_pages) * 8) / 1024 as DataSpaceMB
+	p.data_compression_desc as [compression],
+	b.avg_free_space_in_bytes,
+	b.avg_row_count,
+	b.pages_in_buffer
 FROM sys.tables t
 JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
 JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
 JOIN sys.allocation_units a ON p.partition_id = a.container_id
 JOIN sys.columns c ON c.object_id = t.object_id
+JOIN sys.types ty ON c.system_type_id = ty.system_type_id
+LEFT JOIN [buffer] b ON a.allocation_unit_id = b.allocation_unit_id
 WHERE 
     t.NAME NOT LIKE 'dt%' AND
     i.OBJECT_ID > 255 AND   
     i.index_id <= 1 AND
-	c.system_type_id IN (SELECT system_type_id
-						FROM sys.types
-						WHERE name IN (N'text', N'image', N'ntext'))
---GROUP BY 
---    t.NAME, i.object_id, i.index_id, i.name, p.[Rows]
+	ty.name IN (N'varchar', N'nvarchar', N'varbinary') AND 
+	c.max_length = -1
 ORDER BY 
     TableName
 OPTION (RECOMPILE, MAXDOP 1);
