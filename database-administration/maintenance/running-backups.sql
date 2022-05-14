b0e8a924d2c1beda51f3ea8b8640c0176a91d554
Rudi Bruchez <rudi@babaluga.com>
2022-05-14 08:24:03 +0200

    running backup

---
+++ database-administration/maintenance/running-backups.sql
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------
+-- Get running backup and percent complete
+--
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+select DB_NAME(s.database_id) as db, 
+	r.command,
+	CAST(r.start_time as DATETIME2(0)) as start_time,
+	r.last_wait_type,
+	r.wait_type,
+	r.wait_time,
+	CAST(r.percent_complete as DECIMAL (5,2)) as percent_complete
+from sys.dm_exec_requests r
+join sys.dm_exec_sessions s ON r.session_id = s.session_id
+where s.session_id > 50
+and r.command IN ('BACKUP DATABASE')
+OPTION (RECOMPILE, MAXDOP 1);
