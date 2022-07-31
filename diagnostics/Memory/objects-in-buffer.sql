8997968bc81a0690770fc8aa8b6ab21ca5fca7c1
Rudi Bruchez <rudi@babaluga.com>
2022-07-31 10:19:45 +0200

    reorg

---
+++ diagnostics/Memory/objects-in-buffer.sql
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------
+-- lists objects in buffer for the current database
+--
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+SELECT 
+	MIN(o.name) AS tbl,
+	MIN(i.name) AS idx,
+	b.allocation_unit_id,
+	COUNT(*) as pages,
+	CAST(COUNT(*) * 8.0 / 1024 AS DECIMAL(20, 2)) AS mb
+FROM sys.dm_os_buffer_descriptors b 
+LEFT JOIN sys.allocation_units au
+		ON b.allocation_unit_id = au.allocation_unit_id
+	LEFT JOIN sys.partitions p
+		ON (au.type IN (1,3) AND au.container_id = p.hobt_id)
+		OR (au.type = 2 AND au.container_id = p.partition_id)
+	LEFT JOIN sys.indexes i
+		ON p.object_id = i.object_id AND p.index_id = i.index_id
+	LEFT JOIN sys.objects o
+		ON i.object_id = o.object_id
+WHERE b.database_id = DB_ID()
+GROUP BY b.allocation_unit_id --WITH ROLLUP
+ORDER BY pages DESC
+OPTION (RECOMPILE, MAXDOP 1);
--- hadr/automatic-seeding/monitoring-on-principal.sql
+++ hadr/automatic-seeding/monitoring-on-principal.sql
@@ -5,17 +5,20 @@
 -----------------------------------------------------------------
 
 SELECT 
-    ag.name,
+    ag.name as [AG],
 	pss.local_database_name AS local_database, 
     pss.role_desc as [role], 
     pss.internal_state_desc AS internal_state, 
     CAST(pss.transfer_rate_bytes_per_second / 1024.0 / 1024 AS DECIMAL(10, 2)) AS transfer_rate_MB_second, 
     CAST(pss.transferred_size_bytes / 1024.0 / 1024 AS DECIMAL(10, 2)) AS transferred_size_MB, 
     CAST(pss.database_size_bytes / 1024.0 / 1024 AS DECIMAL(10, 2)) AS database_size_MB, 
+    CAST(1.0 * pss.transferred_size_bytes / pss.database_size_bytes * 100 as numeric(5, 2)) as [%],
     DATEADD(mi, DATEDIFF(mi, GETUTCDATE(), GETDATE()), pss.start_time_utc) as start_time,
+    DATEDIFF(mi, pss.start_time_utc, COALESCE(GETUTCDATE(), pss.end_time_utc)) as minutes_elapsed,
     DATEADD(mi, DATEDIFF(mi, GETUTCDATE(), GETDATE()), pss.end_time_utc) as end_time,
     DATEADD(mi, DATEDIFF(mi, GETUTCDATE(), GETDATE()), pss.estimate_time_complete_utc) as estimate_time_complete, 
     pss.total_disk_io_wait_time_ms, 
+    pss.total_disk_io_wait_time_ms / 1000 / 60 as io_waits_minutes, 
     pss.total_network_wait_time_ms, 
     pss.is_compression_enabled 
 FROM sys.dm_hadr_physical_seeding_stats pss
