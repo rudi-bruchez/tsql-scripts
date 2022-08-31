5fad4079518359d440e32e1a2e2ad2fb8f827f41
Rudi Bruchez <rudi@babaluga.com>
2022-08-31 10:04:03 +0200

    sqlagent

---
+++ database-administration/sqlagent/jobs-history.sql
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------
+-- get jobs and step history for steps that ran for more
+-- than one minute.
+--
+-- rudi@babaluga.com, go ahead license
+-----------------------------------------------------------------
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+;WITH cte AS (
+	SELECT 
+		j.name as JobName,
+		h.step_name as step,
+		h.run_status,
+		msdb.dbo.agent_datetime(run_date, run_time) as RunDateTime,
+		((run_duration/10000*3600 + (run_duration/100)%100*60 + run_duration%100 + 31 ) / 60) 
+		as RunDurationMinutes
+	FROM msdb.dbo.sysjobs j
+	JOIN msdb.dbo.sysjobhistory h ON j.job_id = h.job_id 
+	WHERE j.enabled = 1
+	AND h.step_name NOT IN (N'(Job outcome)')
+)
+SELECT *
+FROM cte
+WHERE RunDurationMinutes > 0
+ORDER BY JobName, RunDateTime DESC
+OPTION (RECOMPILE, MAXDOP 1);
