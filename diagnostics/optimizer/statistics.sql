0e8a1b6977d37ceab7f7054631d877499ef029c2
Rudi Bruchez <rudi@babaluga.com>
2022-09-10 13:24:58 +0200

    stats

--- diagnostics/optimizer/statistics.sql
+++ diagnostics/optimizer/statistics.sql
@@ -4,6 +4,9 @@
 -- rudi@babaluga.com, go ahead license
 -------------------------------------------------------------------------------
 
+DECLARE @tablename sysname = '%';
+DECLARE @onlyAuto bit = 0; -- analyze only auto statistics
+
 SELECT 
 	OBJECT_NAME(stat.object_id) as tbl,
 	stat.object_id,
@@ -14,7 +17,10 @@ SELECT
 	sp.rows_sampled, 
 	sp.steps, 
 	sp.unfiltered_rows, 
-	sp.modification_counter
+	sp.modification_counter,
+	CONCAT('UPDATE STATISTICS ', QUOTENAME(SCHEMA_NAME(o.schema_id)), '.', QUOTENAME(o.name),
+        ' ', stat.name, ' WITH FULLSCAN'
+    ) as ddl
 FROM sys.stats AS stat
 JOIN sys.stats_columns sc ON stat.object_id = sc.object_id AND stat.stats_id = sc.stats_id
 JOIN sys.columns c ON sc.object_id = c.object_id AND sc.column_id = c.column_id
@@ -22,7 +28,7 @@ JOIN sys.columns c ON sc.object_id = c.object_id AND sc.column_id = c.column_id
 JOIN sys.objects o ON stat.object_id = o.object_id
 CROSS APPLY sys.dm_db_stats_properties(stat.object_id, stat.stats_id) AS sp 
 WHERE o.is_ms_shipped = 0
--- AND stat.object_id = OBJECT_ID('<TABLE NAME>')   -- uncomment this line if you want to target a specific table
--- AND stat.name LIKE '_WA_Sys_%'   -- uncomment this line if you want to analyze only auto statistics
+AND o.name LIKE @tablename
+AND (stat.name LIKE '_WA_Sys_%' OR @onlyAuto = 0)
 ORDER BY tbl, column_name, stat.name
 OPTION (RECOMPILE, MAXDOP 1);
