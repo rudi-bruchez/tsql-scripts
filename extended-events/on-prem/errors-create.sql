d8192b4dd22d601f0a0ae2f5a765b678cfd78cba
Rudi Bruchez <rudi@babaluga.com>
2022-09-25 12:49:30 +0200

    improvements

--- extended-events/on-prem/errors-create.sql
+++ extended-events/on-prem/errors-create.sql
@@ -16,6 +16,8 @@ ADD EVENT sqlserver.error_reported(
 		sqlserver.username
 	)
     WHERE ([severity]>(10))
+	-- TODO : error_number <> 17830
+	-- Network error code 0x2746 occurred while establishing a connection; the connection has been closed. This may have been caused by client or server login timeout expiration. Time spent during login: total 0 ms, enqueued 0 ms, network writes 0 ms, network reads 0 ms, establishing SSL 0 ms, network reads during SSL 0 ms, network writes during SSL 0 ms, secure calls during SSL 0 ms, enqueued during SSL 0 ms, negotiating SSPI 0 ms, network reads during SSPI 0 ms, network writes during SSPI 0 ms, secure calls during SSPI 0 ms, enqueued during SSPI 0 ms, validating login 0 ms, including user-defined login processing 0 ms. [CLIENT: ::1]
 	) 
 ADD TARGET package0.event_file(SET filename=N'errors.xel', max_file_size=(50))
 -- ADD TARGET package0.ring_buffer
