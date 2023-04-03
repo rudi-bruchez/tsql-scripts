5268a269c822cf7ae90190393054c0faa521eb50
Rudi Bruchez <rudi@babaluga.com>
2023-04-03 14:17:57 +0200

    mssing index ddl

--- index-management/missing-indexes.sql
+++ index-management/missing-indexes.sql
@@ -4,7 +4,11 @@
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
 
+-------------------------------------------------------
 DECLARE @table_name sysname = '%';
+DECLARE @compressionType varchar(10) = 'ROW';
+DECLARE @online bit = 1;
+-------------------------------------------------------
 
 SELECT	
 	object_name(d.object_id) as [table], 
@@ -16,10 +20,14 @@ SELECT
 	s.avg_user_impact,
 	s.user_seeks + s.user_scans as usage,
 	CAST(COALESCE(s.last_user_seek, s.last_user_scan) as datetime2(0)) as last_usage,
-	'CREATE INDEX nix$' + lower(object_name(object_id)) + '$' 
-	+ REPLACE(REPLACE(REPLACE(COALESCE(equality_columns, inequality_columns), ']', ''), '[', ''), ', ', '_')
-	+ ' ON ' + statement + ' (' + COALESCE(equality_columns, inequality_columns) 
-	+ COALESCE(') INCLUDE (' + included_columns, '') + ') WITH (ONLINE = ON, DATA_COMPRESSION = ROW, SORT_IN_TEMPDB = ON)' as [DDL]
+
+	-- DDL to create the index
+	CONCAT('CREATE INDEX nix$', lower(object_name(object_id)), '$' 
+	, REPLACE(REPLACE(REPLACE(COALESCE(equality_columns + ', ' + inequality_columns, equality_columns, inequality_columns), ']', ''), '[', ''), ', ', '_')
+	, ' ON ', statement, ' (', COALESCE(equality_columns + ', ' + inequality_columns, equality_columns, inequality_columns) 
+	, COALESCE(') INCLUDE (' + included_columns, '')
+	, ') WITH (', IIF(@online = 1, 'ONLINE = ON', ''), ', DATA_COMPRESSION = ', @compressionType, ', SORT_IN_TEMPDB = ON)') as [DDL]
+
 FROM	sys.dm_db_missing_index_details d 
 JOIN sys.dm_db_missing_index_groups g ON d.index_handle = g.index_handle
 JOIN sys.dm_db_missing_index_group_stats s ON g.index_group_handle = s.group_handle
@@ -27,3 +35,4 @@ WHERE database_id = db_id()
 AND object_name(d.object_id) LIKE @table_name
 ORDER BY usage DESC, object_name(d.object_id), s.user_seeks DESC, d.object_id
 OPTION (RECOMPILE, MAXDOP 1);
+
