dee513b4f05fe9c71ddd71883c3de85dcc717510
Rudi Bruchez <rudi@babaluga.com>
2024-04-11 16:18:50 +0200

    partition

--- database-administration/ddl-generation/change-database-owner.sql
+++ database-administration/ddl-generation/change-database-owner.sql
@@ -11,9 +11,11 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SELECT 
 	name as db, 
 	SUSER_SNAME(owner_sid) as [owner],
-	CONCAT('ALTER AUTHORIZATION ON DATABASE::', QUOTENAME(name), ' TO [sa]') as ddl
+	CONCAT('ALTER AUTHORIZATION ON DATABASE::', QUOTENAME(name), ' TO [sa];') as ddl,
+	CONCAT('ALTER AUTHORIZATION ON DATABASE::', QUOTENAME(name), ' TO ', QUOTENAME(SUSER_SNAME(owner_sid)) , ';') as ddl_back
 FROM sys.databases
 WHERE database_id > 4
 AND owner_sid <> 0x01
+--AND SUSER_SNAME(owner_sid) IS NULL
 ORDER BY db
 OPTION (RECOMPILE, MAXDOP 1);
--- database-information/size-and-allocation/partitioned-objects-by-partition-function.sql
+++ database-information/size-and-allocation/partitioned-objects-by-partition-function.sql
@@ -1,31 +1,27 @@
 -----------------------------------------------------------------
--- List partitioned tables and other info about them
--- adapted from https://www.sqlrx.com/list-partitioned-tables-and-other-info-about-them/
+-- Find all objects partitioned on a specific partition function
 --
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
-DECLARE @table_name sysname = '%';
+DECLARE @partition_function sysname = N'<function name here>';
 
 SET NOCOUNT ON;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 SELECT 
 	SCHEMA_NAME(o.schema_id) + '.' + OBJECT_NAME(i.object_id) AS [object]
-	,p.partition_number AS [p]
-	,fg.name AS [fg]
-	,p.rows
-	,au.total_pages AS pages
-	,CASE boundary_value_on_right
-	WHEN 1 THEN '<'
-	ELSE '<=' END AS comparison
-	,rv.value
-	,i.name as [index]
-	,i.index_id
-	,i.type_desc AS [index_type]
-	,au.type_desc AS [alloc_type]
-	,FORMAT((au.total_pages * 8.0) / 1024, 'n') AS mb_total
-	,FORMAT((au.used_pages * 8.0) / 1024, 'n') AS mb_used
-	,p.data_compression_desc AS [compression]
+	,FORMAT((SUM(p.rows) * 8.0) / 1024, 'n') AS [rows]
+	,FORMAT((SUM(au.total_pages) * 8.0) / 1024, 'n') AS mb_total
+	,FORMAT((SUM(au.used_pages) * 8.0) / 1024, 'n') AS mb_used
+	,CASE i.index_id
+		WHEN 0 THEN '(heap)'
+		WHEN 1 THEN 'clustered'
+		ELSE MIN(i.name) 
+	END as [index]
+	,i.index_id as index_id
+	--,MIN(i.type_desc) as idx_type
+	,MIN(au.type_desc) as alloc_type
+	,MIN(p.data_compression_desc) as [compression]
 FROM sys.partitions p JOIN sys.indexes i ON p.object_id = i.object_id AND p.index_id = i.index_id
 JOIN sys.objects o ON p.object_id = o.object_id
 JOIN sys.allocation_units au ON p.partition_id = au.container_id
@@ -36,6 +32,7 @@ JOIN sys.destination_data_spaces dds ON dds.partition_scheme_id = ps.data_space_
 JOIN sys.filegroups fg ON dds.data_space_id = fg.data_space_id
 LEFT JOIN sys.partition_range_values rv ON f.function_id = rv.function_id
     AND p.partition_number = rv.boundary_id
-WHERE CONCAT(SCHEMA_NAME(o.schema_id), '.', OBJECT_NAME(i.object_id)) LIKE @table_name
-ORDER BY o.name, i.index_id, p.partition_number
-OPTION (RECOMPILE, MAXDOP 1);
+WHERE f.name = @partition_function
+GROUP BY o.schema_id, i.object_id, i.index_id
+ORDER BY [object], index_id
+OPTION (RECOMPILE, MAXDOP 1); 
