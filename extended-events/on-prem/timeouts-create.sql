2aa2fde0de775633c3982e1baca65b2fee69b50a
Rudi Bruchez <rudi@babaluga.com>
2024-07-20 12:11:17 +0200

    2024.07

--- extended-events/on-prem/timeouts-create.sql
+++ extended-events/on-prem/timeouts-create.sql
@@ -4,10 +4,6 @@
 -- rudi@babaluga.com, go ahead license
 -----------------------------------------------------------------
 
-SET NOCOUNT ON;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-
-
 CREATE EVENT SESSION [timeouts] ON SERVER 
 ADD EVENT sqlserver.rpc_completed(SET collect_statement=(1)
     ACTION(
@@ -25,7 +21,7 @@ ADD EVENT sqlserver.sql_batch_completed(
         sqlserver.sql_text,
         sqlserver.username)
     WHERE ([result]=(2)))
-ADD TARGET package0.event_file(SET filename=N'timeouts')
+ADD TARGET package0.event_file(SET filename=N'timeouts', max_file_size=(100))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=ON)
 GO
 
