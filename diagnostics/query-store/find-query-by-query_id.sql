abebcdc5353c2e3fc7b1be7c1389d966ed5e3c71
Rudi Bruchez <rudi@babaluga.com>
2024-11-07 13:23:40 +0100

    improvements

--- diagnostics/query-store/find-query-by-query_id.sql
+++ diagnostics/query-store/find-query-by-query_id.sql
@@ -10,8 +10,8 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 DECLARE @query_id BIGINT = 1028;
 
 SELECT
-	dest.text
+	COALESCE(dest.text, 'NULL: is the QS read-only?') as query_text
 FROM sys.query_store_query qsq
-CROSS APPLY sys.dm_exec_sql_text(qsq.batch_sql_handle) dest
+OUTER APPLY sys.dm_exec_sql_text(qsq.batch_sql_handle) dest
 WHERE qsq.query_id = @query_id
 OPTION (RECOMPILE, MAXDOP 1);
