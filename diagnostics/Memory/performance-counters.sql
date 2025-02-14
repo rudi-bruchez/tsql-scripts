4ec2044ba11f958662336f62257b63443e2eee6f
Rudi Bruchez <rudi@babaluga.com>
2025-02-14 12:17:13 +0100

    PLE

--- diagnostics/Memory/performance-counters.sql
+++ diagnostics/Memory/performance-counters.sql
@@ -7,17 +7,25 @@
 SET NOCOUNT ON;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
-SELECT N'Buffer cache hit ratio' AS counter_name, CAST((ratio.cntr_value * 1.0 / base.cntr_value) * 100.0 AS NUMERIC(5, 2)) as [Value]
-FROM sys.dm_os_performance_counters ratio WITH (READUNCOMMITTED)
-JOIN sys.dm_os_performance_counters base  WITH (READUNCOMMITTED) 
+SELECT N'Buffer cache hit ratio' AS counter_name, 
+CAST(CAST((ratio.cntr_value * 1.0 / base.cntr_value) * 100.0 AS NUMERIC(5, 2)) as NVARCHAR(100)) as [Value]
+FROM sys.dm_os_performance_counters ratio
+JOIN sys.dm_os_performance_counters base 
 	ON ratio.object_name = base.object_name
 WHERE RTRIM(ratio.object_name) LIKE N'%:Buffer Manager'
 AND ratio.counter_name = N'Buffer cache hit ratio'
 AND base.counter_name = N'Buffer cache hit ratio base'
 UNION ALL
+SELECT CONCAT(TRIM([counter_name]), ' per NUMA') as counter_name, 
+	STRING_AGG(CONCAT('NUMA ', TRIM([instance_name]), ' : ', [cntr_value]), ',') as [Value]
+FROM sys.dm_os_performance_counters
+WHERE [object_name] LIKE '%SQLServer:Buffer Node%'
+AND [counter_name] = 'Page life expectancy'
+GROUP BY [counter_name]
+UNION ALL 
 SELECT 
 	RTRIM(counter_name) as counter_name, 
-	cntr_value AS [value]
+	CAST(cntr_value as NVARCHAR(100)) AS [value]
 FROM sys.dm_os_performance_counters WITH (READUNCOMMITTED)
 WHERE (RTRIM([object_name]) LIKE N'%:Buffer Manager' -- Handle named instances
 	AND counter_name IN (N'Page life expectancy'))
