71aa27871206853658fd81aa5cdf0a811c12fc54
Rudi Bruchez <rudi@babaluga.com>
2025-05-02 20:58:51 +0200

    small improvements

--- database-information/statistics/statistics.sql
+++ database-information/statistics/statistics.sql
@@ -6,6 +6,8 @@
 
 DECLARE @tablename sysname = '%';
 DECLARE @onlyAuto bit = 0; -- analyze only auto statistics
+DECLARE @onlyModified bit = 0; -- analyze only statistics with modified rows
+
 
 SELECT 
 	CONCAT(QUOTENAME(SCHEMA_NAME(o.schema_id)), '.', 
@@ -35,5 +37,6 @@ WHERE o.is_ms_shipped = 0
 AND o.name LIKE @tablename
 -- AND (stat.name LIKE '_WA_Sys_%' OR @onlyAuto = 0)
 AND (stat.auto_created = 1 OR @onlyAuto = 0)
+AND (sp.modification_counter > 0 OR @onlyModified = 0)
 ORDER BY tbl, column_name, stat.name
 OPTION (RECOMPILE, MAXDOP 1);
